8ee59876a7db45b6a62fd9b01d8ceaf83d40bcad
William Stewart <william.stewart@booking.com>
2018-11-21 11:42:23 +0100

    Simpler, better, faster

--- pkg/chartutil/values.go
+++ pkg/chartutil/values.go
@@ -496,41 +496,14 @@ func readFile(settings environment.EnvSettings, filePath, CertFile, KeyFile, CAF
 	return data.Bytes(), err
 }
 
-// Merges source and destination map, preferring values from the source map
-func mergeValues(dest map[string]interface{}, src map[string]interface{}) map[string]interface{} {
-	for k, v := range src {
-		// If the key doesn't exist already, then just set the key to that value
-		if _, exists := dest[k]; !exists {
-			dest[k] = v
-			continue
-		}
-		nextMap, ok := v.(map[string]interface{})
-		// If it isn't another map, overwrite the value
-		if !ok {
-			dest[k] = v
-			continue
-		}
-		// Edge case: If the key exists in the destination, but isn't a map
-		destMap, isMap := dest[k].(map[string]interface{})
-		// If the source map has a map for this key, prefer it
-		if !isMap {
-			dest[k] = v
-			continue
-		}
-		// If we got to this point, it is a map in both, so merge them
-		dest[k] = mergeValues(destMap, nextMap)
-	}
-	return dest
-}
-
 // MergeValuesFiles merges values from files specified via -f/--values and
 // directly via --set or --set-string or --set-file, marshaling them to YAML
 func MergeValuesFiles(settings environment.EnvSettings, valueFiles ValueFiles, values []string, stringValues []string, fileValues []string, CertFile, KeyFile, CAFile string) ([]byte, error) {
-	base := map[string]interface{}{}
+	base := make(Values)
 
 	// User specified a values files via -f/--values
 	for _, filePath := range valueFiles {
-		currentMap := map[string]interface{}{}
+		currentMap := make(Values)
 
 		var bytes []byte
 		var err error
@@ -548,7 +521,7 @@ func MergeValuesFiles(settings environment.EnvSettings, valueFiles ValueFiles, v
 			return []byte{}, fmt.Errorf("failed to parse %s: %s", filePath, err)
 		}
 		// Merge with the previous map
-		base = mergeValues(base, currentMap)
+		base.MergeInto(currentMap)
 	}
 
 	// User specified a value via --set
